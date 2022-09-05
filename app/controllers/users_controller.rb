5ef4e4bc1d5e131d17bff1b397f4a0f1bdbb1670
Tomer Harari <tharari21@gmail.com>
2022-09-05 15:51:43 -0400

    commit tree b72c73f6a85ced76ebcd24b52c8e4739c1f67d2d

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+  def show
+    return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include? :user_id
+    user = User.find(session[:user_id])
+    render json: user
+  end
+  private
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
